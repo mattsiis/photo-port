be0bcf817ccadf1ae5442ee4d92bbd2d57f6d1d9
mattsiis <35969076+mattsiis@users.noreply.github.com>
2022-05-07 01:21:15 -0400

    unkonwn feature

--- src/App.js
+++ src/App.js
@@ -29,7 +29,7 @@ function App() {
         setCurrentCategory={setCurrentCategory}
         currentCategory={currentCategory}></Nav>
       <main>
-        <Gallery></Gallery>
+        <Gallery currentCategory={currentCategory}></Gallery>
         <About></About>
       </main>
     </div>
